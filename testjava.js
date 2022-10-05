7fbbe8103133b2e23468d0b0d09f294ab1a49bd3
eclampitt <91210072+eclampitt@users.noreply.github.com>
2022-10-04 21:27:55 -0500

    Add files via upload

--- testjava.js
+++ testjava.js
@@ -139,7 +139,7 @@ function scrollFunction() {
     nav.style.transition  = "all .25s";
     nav.style.boxShadow = 'none';
     nav.style.backgroundColor = 'transparent';
-    nav.style.color = 'var(--bluefont-bg)';
+    nav.style.color = 'var(--darkfont-bg)';
     btnlinearray.forEach((array) => {
       array.style.backgroundColor = 'var(--lightfont-bg)';
     });  
