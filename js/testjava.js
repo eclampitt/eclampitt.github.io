d9258628c98d9d01fee80547bbb651288008067f
eclampitt <91210072+eclampitt@users.noreply.github.com>
2022-10-19 09:47:05 -0500

    Add files via upload

--- js/testjava.js
+++ js/testjava.js
@@ -12,6 +12,50 @@ Section 9: Media Devices Content Styling
 ************************************/
 
 /************************************
+Section 5: Scroll Reveal
+
+https://codepen.io/ShuvoK/pen/VwMvbbg
+************************************/
+
+function scrollTrigger(selector, options = {}){
+  let els = document.querySelectorAll(selector)
+  els = Array.from(els)
+  els.forEach(el => {
+      addObserver(el, options)
+  })
+}
+
+function addObserver(el, options){
+  if(!('IntersectionObserver' in window)){
+      if(options.cb){
+          options.cb(el)
+      }else{
+          entry.target.classList.add('active')
+      }
+      return
+  }
+  let observer = new IntersectionObserver((entries, observer) => { //this takes a callback function which receives two arguments: the elemts list and the observer instance
+      entries.forEach(entry => {
+          if(entry.isIntersecting){
+              if(options.cb){
+                  options.cb(el)
+              }else{
+                  entry.target.classList.add('active')
+              }
+              observer.unobserve(entry.target)
+          }
+      })
+  }, options)
+  observer.observe(el)
+}
+// Example usages:
+
+scrollTrigger('.scroll-reveal', {
+  rootMargin: '-100px',
+})
+
+
+/************************************
 Section 1: Hamburger Menu
 ************************************/
 const menuBtn = document.querySelector(".menu-btn");
@@ -288,45 +332,3 @@ function templatedescr() {
 
 */
 
-/************************************
-Section 5: Scroll Reveal
-
-https://codepen.io/ShuvoK/pen/VwMvbbg
-************************************/
-
-function scrollTrigger(selector, options = {}){
-  let els = document.querySelectorAll(selector)
-  els = Array.from(els)
-  els.forEach(el => {
-      addObserver(el, options)
-  })
-}
-
-function addObserver(el, options){
-  if(!('IntersectionObserver' in window)){
-      if(options.cb){
-          options.cb(el)
-      }else{
-          entry.target.classList.add('active')
-      }
-      return
-  }
-  let observer = new IntersectionObserver((entries, observer) => { //this takes a callback function which receives two arguments: the elemts list and the observer instance
-      entries.forEach(entry => {
-          if(entry.isIntersecting){
-              if(options.cb){
-                  options.cb(el)
-              }else{
-                  entry.target.classList.add('active')
-              }
-              observer.unobserve(entry.target)
-          }
-      })
-  }, options)
-  observer.observe(el)
-}
-// Example usages:
-
-scrollTrigger('.scroll-reveal', {
-  rootMargin: '-100px',
-})
