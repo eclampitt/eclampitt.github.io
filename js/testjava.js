532e26c3ca109d5bf9ef15001c1a2b1ef2115b8c
eclampitt <91210072+eclampitt@users.noreply.github.com>
2022-10-28 14:49:38 -0500

    Add files via upload

--- js/testjava.js
+++ js/testjava.js
@@ -211,7 +211,7 @@ Section 5: Test JS - Process Load container
 
 *Delete if Not Working*
 ************************************/
-
+/*
 const arrowl = document.getElementById("arrow-left");
 const arrowricon = document.getElementById("arrow-right-icon");
 const arrowlicon = document.getElementById("arrow-left-icon");
@@ -278,7 +278,7 @@ function arrows() {
     }
 
 }
-
+*/
 /************************************
 Section 5: Test JS - Benefits
 ************************************/
@@ -331,3 +331,64 @@ function templatedescr() {
 }
 
 */
+
+
+
+/************************************
+SVG Moving
+************************************/
+
+(function( $ ) {
+
+	'use strict';
+
+    /*
+    SVG Morphing
+    */
+    if( $('#shape_one_start').get(0) ) {
+	    var shape1 = KUTE.fromTo('#shape_one_start', {
+			path: '#shape_one_start' 
+		}, { 
+			path: '#shape_one_end' 
+		}, {
+			duration: 10000,
+			easing	: 'easingQuadraticInOut',
+			repeat: 20,
+			repeatDelay: 1000,
+			yoyo: true
+		}).start();
+	}
+
+	if( $('#shape_two_start').get(0) ) {
+		var shape2 = KUTE.fromTo('#shape_two_start', {
+			path: '#shape_two_start' 
+		}, { 
+			path: '#shape_two_end' 
+		}, {
+			duration: 10000,
+			easing	: 'easingQuadraticInOut',
+			repeat: 20,
+			repeatDelay: 1000,
+			yoyo: true
+		}).start();
+	}
+
+	/*
+	Form
+	*/
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+		mainClass: 'my-mfp-zoom-in',
+		callbacks: {
+			open: function() {
+				$('html').addClass('lightbox-opened');
+			},
+			close: function() {
+				$('html').removeClass('lightbox-opened');
+			}
+		}
+	});
+
+}).apply( this, [ jQuery ]);
